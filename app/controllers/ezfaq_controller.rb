1335d4d0b3ef500bc166ff47ffd53502d34ab830
NAITOH Jun <naitoh@gmail.com>
2013-02-21 20:19:41 +0900

    Redmine PDF export support

--- app/controllers/ezfaq_controller.rb
+++ app/controllers/ezfaq_controller.rb
@@ -204,7 +204,7 @@ private
   def faq_to_pdf
     faq_setting = FaqSetting.find(:first, :conditions => "project_id = #{@project.id}")
 
-    pdf = IFPDF.new(current_language)
+    pdf = ITCPDF.new(current_language)
     pdf.SetTitle("#{l(:label_faq)}-#{@faq.question}")
     pdf.SetAuthor('ezFAQ for Redmine')
     pdf.AliasNbPages
@@ -212,27 +212,31 @@ private
     pdf.AddPage
     
     pdf.SetFontStyle('B',16)
-    pdf.Cell(200,5, faq_setting.pdf_title)
+    if (faq_setting && faq_setting.pdf_title)
+      pdf.RDMCell(200,5, faq_setting.pdf_title)
+    else
+      pdf.RDMCell(200,5, l(:text_faq_pdf_title_not_set))
+    end
     pdf.Ln(15)
 
     pdf.SetFontStyle('B',11)
-    pdf.Cell(200,5, "#{l(:field_question)}: #{@faq.question}")
+    pdf.RDMCell(200,5, "#{l(:field_question)}: #{@faq.question}")
     pdf.Ln
     pdf.Line(pdf.GetX, pdf.GetY, 180, pdf.GetY)
     pdf.Ln
     pdf.SetFontStyle('',11)
-    pdf.MultiCell(200,5, @faq.answer)
+    pdf.RDMMultiCell(200,5, @faq.answer)
     pdf.Ln
 
     pdf.Line(pdf.GetX, pdf.GetY, 100, pdf.GetY)
     pdf.SetFontStyle('I',8)
-    pdf.Cell(200,5, 'Auto generated faq-document by ezFAQ. Powered by ezWORK & Redmine.')
+    pdf.RDMCell(200,5, 'Auto generated faq-document by ezFAQ. Powered by ezWORK & Redmine.')
 
     pdf.Output
   end
 
   def faqs_to_pdf
-    pdf = IFPDF.new(current_language)
+    pdf = ITCPDF.new(current_language)
     pdf.SetTitle(@faq_setting.pdf_title) if (@faq_setting && @faq_setting.pdf_title)
     pdf.SetAuthor('ezFAQ for Redmine')
     pdf.AliasNbPages
@@ -241,14 +245,14 @@ private
 
     pdf.SetFontStyle('B',16)
     if (@faq_setting && @faq_setting.pdf_title)
-      pdf.Cell(200,5, @faq_setting.pdf_title)
+      pdf.RDMCell(200,5, @faq_setting.pdf_title)
     else
-      pdf.Cell(200,5, l(:text_faq_pdf_title_not_set))
+      pdf.RDMCell(200,5, l(:text_faq_pdf_title_not_set))
     end
     
     pdf.Ln(10)
     pdf.SetFontStyle('',11)
-    pdf.MultiCell(180,5, @faq_setting.note) if (@faq_setting && @faq_setting.note)
+    pdf.RDMMultiCell(180,5, @faq_setting.note) if (@faq_setting && @faq_setting.note)
     pdf.Ln
     pdf.Line(pdf.GetX, pdf.GetY, 180, pdf.GetY)
     pdf.Ln
@@ -257,15 +261,15 @@ private
     if @categorized_faqs.any?
       @categorized_faqs.group_by(&:category).sort.each do |category, faqs|
         pdf.SetFontStyle('BI', 13)
-        pdf.Cell(200,5, "#{list_number}. #{category.name}")
+        pdf.RDMCell(200,5, "#{list_number}. #{category.name}")
         pdf.Ln
         faq_number = 1
         for faq in faqs.sort
           pdf.SetFontStyle('B',11)
-          pdf.Cell(200,5, "#{list_number}.#{faq_number} #{faq.question}")
+          pdf.RDMCell(200,5, "#{list_number}.#{faq_number} #{faq.question}")
           pdf.Ln
           pdf.SetFontStyle('',11)
-          pdf.MultiCell(200,5, faq.answer)
+          pdf.RDMMultiCell(200,5, faq.answer)
           pdf.Ln
           faq_number += 1
         end
@@ -276,15 +280,15 @@ private
     
     if @not_categorized_faqs.any?
       pdf.SetFontStyle('BI', 13)
-      pdf.Cell(200,5, "#{list_number}. #{l(:label_not_categorized)}")
+      pdf.RDMCell(200,5, "#{list_number}. #{l(:label_not_categorized)}")
       pdf.Ln
       faq_number = 1
       for faq in @not_categorized_faqs.sort
         pdf.SetFontStyle('B',11)
-        pdf.Cell(200,5, "#{list_number}.#{faq_number} #{faq.question}")
+        pdf.RDMCell(200,5, "#{list_number}.#{faq_number} #{faq.question}")
         pdf.Ln
         pdf.SetFontStyle('',11)
-        pdf.MultiCell(200,5, faq.answer)
+        pdf.RDMMultiCell(200,5, faq.answer)
         pdf.Ln(10)
         faq_number += 1
       end      
@@ -292,7 +296,7 @@ private
 
     pdf.Line(pdf.GetX, pdf.GetY, 100, pdf.GetY)
     pdf.SetFontStyle('I',8)
-    pdf.Cell(200,5, 'Auto generated faq-list by ezFAQ. Powered by ezWORK & Redmine.')
+    pdf.RDMCell(200,5, 'Auto generated faq-list by ezFAQ. Powered by ezWORK & Redmine.')
 
     pdf.Output
   end
